ca59b40224f1df0da9855eee47a54184702c0d3a
wambura001 <meshack.sirincha@student.moringaschool.com>
2021-07-15 22:06:54 +0300

    add HTML,README.md and scripts.js files

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var add = (number1, number2) => number1 + number2
+var number1 = parseInt (prompt ("Enter a number"))
+var number2 = parseInt (prompt ("Enter another number"))
+alert(add(number1,number2));
+
